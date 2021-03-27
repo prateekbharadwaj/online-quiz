35a7f90f726aa9c36ac76acc934668f9c807bc7e
prateekbharadwaj <prateekbharadwaj2533@gmail.com>
2021-03-27 13:24:38 +0530

    initial commit

---
+++ signup1.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+
+
+if(isset($_POST["1"]))
+{
+	$name=$_POST["name"];
+	$email=$_POST["email1"];
+	$dob=$_POST["dob"];
+	$gender=$_POST["gender"];
+	$mobile=$_POST["mobile"];
+	$password=$_POST["password"];
+	$branch=$_POST["branch"];
+	$insti=$_POST["institute"];
+
+	$con=mysqli_connect("localhost","root","","oq") or die("database not found");
+$query= "insert into registration set name='$name', email='$email', 
+    dob='$dob', mobile='$mobile', branch='$branch',gender='$gender', password='$password', institute='$insti' ";
+$x=	mysqli_query($con,$query) or die(header("Location:index.php?err=1"));
+header("Location:success.php?err=1");
+}
+
+?>
+
